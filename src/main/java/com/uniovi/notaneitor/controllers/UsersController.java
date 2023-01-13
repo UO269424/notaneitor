5d9b51dcd0e8bcbd969818f9ca0bae44839aa0ff
Alonso Gago <UO269424@uniovi.es>
2023-01-13 04:07:38 +0100

    Validación de datos en el servidor

--- src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/UsersController.java
@@ -3,11 +3,14 @@ package com.uniovi.notaneitor.controllers;
 import com.uniovi.notaneitor.entities.User;
 import com.uniovi.notaneitor.services.SecurityService;
 import com.uniovi.notaneitor.services.UsersService;
+import com.uniovi.notaneitor.validators.SignUpFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,8 @@ public class UsersController {
     private UsersService usersService;
     @Autowired
     private SecurityService securityService;
+    @Autowired
+    private SignUpFormValidator signUpFormValidator;
 
 
     @RequestMapping("/user/list")
@@ -59,7 +64,12 @@ public class UsersController {
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user, result);
+        if(result.hasErrors())  {
+            return "signup";
+        }
+
         usersService.addUser(user);
         securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
         return "redirect:home";
@@ -76,6 +86,12 @@ public class UsersController {
         User activeUser = usersService.getUserByDni(dni);
         model.addAttribute("markList", activeUser.getMarks());
         return "home";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
     }
 
 }
